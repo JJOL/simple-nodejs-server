0d9081ab67e287d9be87bd6b2c62f1a3df8bbf97
JJOL <jjoulk@gmail.com>
2016-11-07 20:26:30 -0600

    Likehood

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 
 var app = express();
-var env = {};
 var port = env.port || 8080;
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
   res.send("Hello World We Are Working!!!!");
 });
 
 
-app.listen(port, function() {
+app.listen(app.get('port'), function() {
   console.log('Listening on port: ', port);
 });
